507b74765f8602d57e1c1152cf777bdde8287028
huyenngtn <huyenngtn@bkav.com>
2021-01-07 21:47:53 +0700

    update file storage

--- src/main/java/com/example/crud/service/FilesStorageService.java
+++ src/main/java/com/example/crud/service/FilesStorageService.java
@@ -5,6 +5,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface FilesStorageService {
     void init();
+    
     String save(MultipartFile file);
     Resource load(String filename);
     void deleteAll();
